c8ed0edce2263fd6bb98bda0e6652aea73a536a2
Kristin Henno <kristinhenno@gmail.com>
2019-06-24 20:20:39 -0700

    Cards added

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,32 +1,72 @@
 import React from "react";
 import styled from "styled-components";
+const Card = ({ name }) => {
+  return <div>{name}</div>;
+};
 
 const Events = () => {
-
-
-	return <Style>
-		<h1>Events</h1>
-		<h2>Standard</h2>
-		<div>Content</div>
-	</Style>
-
-
-
-
-
-}
+  return (
+    <Style>
+      <h1>Events</h1>
+      <h2>Standard</h2>
+      <div>
+        <ul>
+          <li>
+            8"
+            <Card
+              name="Fido"
+              breed="Mini Poodle"
+              trainer="Joan White"
+            />
+            <Card
+              name="Rolf"
+              breed="Corgie"
+              trainer="Brenda Smith"
+            />
+            <Card name="Pixie" />
+            <Card name="Brittney" />
+          </li>
+          <li>
+            20"
+            <Card name="Rex" />
+            <Card name="Masterson" />
+            <Card name="Val" />
+          </li>
+          <li>
+            24"
+            <Card name="Thor dog of Thunder" />
+          </li>
+        </ul>
+      </div>
+    </Style>
+  );
+};
 
 export default Events;
 
-
 const Style = styled.section`
-*{margin:0;
-padding:0;}
-outline: 1px solid red;
-height: 95vh;
-display:grid;
-grid-template: "header" 70px "subheader" 67px "content";
-h1{outline:1px solid green};
-h2{outline: 1px solid pink};
-div{outline:1px solid purple};
-`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  outline: 1px solid red;
+  height: 95vh;
+  display: grid;
+  grid-template: "header" 30px "subheader" 32px "content";
+  h1 {
+    outline: 1px solid green;
+  }
+  h2 {
+    outline: 1px solid pink;
+  }
+  div {
+    outline: 1px solid purple;
+    ul {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      li {
+      }
+    }
+  }
+`;
