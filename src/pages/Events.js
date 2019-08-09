342220d5a59ee9c437a525c7661e80354fed7c03
Kristin Henno <kristinhenno@gmail.com>
2019-08-08 18:42:25 -0700

    styled select filters

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -2,12 +2,35 @@ import React from "react";
 import styled from "styled-components";
 import dogs from "../events_stub.json";
 import Card from "../atoms/Card";
+import SelectFilter from "../atoms/SelectFilter";
+
 const jumpHeights = [8, 10, 20, 24];
 
 const Events = () => {
+  const rings = ["Ring 1", "Ring 2"];
+  const levels = ["Masters", "Performance"];
+  const height = ["20", "16"];
+  const events = ["Jumpers", "Gamblers"];
+
   return (
     <Style>
-      <h1> Events </h1> <h2> Standard </h2>
+      <h1> Events </h1>
+      <ul>
+        <li>
+          <SelectFilter filters={rings} />
+        </li>
+        <li>
+          {" "}
+          <SelectFilter filters={levels} />
+        </li>
+        <li>
+          <SelectFilter filters={events} />
+        </li>
+        <li>
+          <SelectFilter filters={height} />
+        </li>
+      </ul>
+      <h2> Standard </h2>
       <div>
         <ul>
           {jumpHeights.map(height => (
@@ -38,9 +61,19 @@ const Style = styled.section`
   height: 95vh;
   width: 100%;
   display: grid;
-  grid-template: "header" 30px "subheader" 32px "content";
+  grid-template: "header" 30px "filters" auto "subheader" 32px "content";
   h1 {
   }
+
+  > ul {
+    display: flex;
+    justify-content: flex-start;
+
+    > li {
+      margin-right: 2rem;
+    }
+  }
+
   h2 {
     margin-left: 1em;
   }
