aadc2e84396bec86564cd21e625713a2fbfb9ce7
Kristin Henno <kristinhenno@gmail.com>
2020-01-08 16:18:49 -0800

    map added to home screen with starting location and destination location

--- src/atoms/Map.js
+++ src/atoms/Map.js
@@ -1,4 +1,11 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  Component
+} from "react";
+import styled from "styled-components";
+import DogPin from "./DogPin3.png";
 
 export default function Map({
   options,
@@ -6,6 +13,9 @@ export default function Map({
   className
 }) {
   const ref = useRef();
+  const [userAddress, setUserAddress] = useState("");
+  const SBbFlyerAddress =
+    "223 E de La Guerra, Santa Barbara, CA, 93101";
 
   useEffect(() => {
     const onLoad = () => {
@@ -13,31 +23,58 @@ export default function Map({
         ref.current,
         options
       );
+
       if (typeof onMount === `function`) onMount(map);
 
+      //geocode to drop pin
       const geocoder = new window.google.maps.Geocoder();
-      const address =
-        "223 E de La Guerra, Santa Barbara, CA, 93101";
 
-      geocoder.geocode({ address: address }, function(
-        results,
-        status
-      ) {
-        if (status == "OK") {
-          console.log(results);
-          map.setCenter(results[0].geometry.location);
-          const marker = new window.google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-          });
-        } else {
-          alert(
-            "Geocode was not successful for the following reason: " +
-              status
-          );
+      const icon = DogPin;
+      geocoder.geocode(
+        { address: SBbFlyerAddress },
+        function(results, status) {
+          if (status === "OK") {
+            console.log(results);
+            map.setCenter(results[0].geometry.location);
+            const marker = new window.google.maps.Marker({
+              map: map,
+              position: results[0].geometry.location,
+              icon: icon,
+              animation: window.google.maps.Animation.DROP
+            });
+            marker.setMap(map);
+          }
         }
-      });
+      );
+
+      // get user location
+
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(
+          position
+        ) {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          console.log(pos);
+
+          //user location to address
+
+          geocoder.geocode({ location: pos }, function(
+            results,
+            status
+          ) {
+            const newAddress = results[0].formatted_address;
+            setUserAddress(newAddress);
+          });
+        });
+      }
+
+      //display directions
     };
+
+    //render map
     if (!window.google) {
       const script = document.createElement(`script`);
       script.src =
@@ -50,19 +87,105 @@ export default function Map({
   }, [onMount, options]);
 
   return (
-    <div
-      style={{
-        height: `60vh`,
-        margin: `1em 0`,
-        borderRadius: `0.5em`
-      }}
-      {...{ ref, className }}
-    />
+    <Style>
+      <div>
+        <div>
+          <input
+            className="controls"
+            id="origin-input"
+            type="text"
+            onChange={e => setUserAddress(e.target.value)}
+            value={userAddress}
+          />
+          <input
+            id="destination-input"
+            className="controls"
+            type="text"
+            value={SBbFlyerAddress}
+          ></input>
+          {/* <button id="submit" onSubmit={getDirections}> */}
+          <button id="submit">Get Directions</button>
+        </div>
+
+        <div
+          id="map"
+          style={{
+            height: `60vh`,
+            margin: `1% 0`,
+            borderRadius: `0.5%`
+          }}
+          {...{ ref, className }}
+        ></div>
+      </div>
+    </Style>
   );
 }
 
 Map.defaultProps = {
   options: {
-    zoom: 5
+    zoom: 5,
+    disableDefaultUI: true
   }
 };
+
+const Style = styled.section`
+  .controls {
+    border: 1px solid transparent;
+    border-radius: 2px 0 0 2px;
+    box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    height: 32px;
+    outline: none;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+  }
+
+  #submit {
+    border: 1px solid transparent;
+    border-radius: 2px 0 0 2px;
+    box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    height: 32px;
+    outline: none;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+    background-color: #4d90fe;
+    font-family: Roboto;
+    font-size: 15px;
+    font-weight: 300;
+    text-overflow: ellipsis;
+    margin-left: 33%;
+    margin-top: 1%;
+    position: absolute;
+    z-index: 1;
+    border-color: #fff;
+    color: #fff;
+  }
+
+  #origin-input,
+  #destination-input {
+    background-color: #fff;
+    font-family: Roboto;
+    font-size: 15px;
+    font-weight: 300;
+    text-overflow: ellipsis;
+  }
+
+  #origin-input {
+    position: absolute;
+    margin-top: 1%;
+    z-index: 1;
+    margin-left: 1%;
+    padding: 0 0.5%;
+    width: 15%;
+    border-color: #4d90fe;
+  }
+
+  #destination-input {
+    position: absolute;
+    margin-top: 1%;
+    z-index: 1;
+    margin-left: 17%;
+    padding: 0 0.5%;
+    width: 15%;
+    border-color: #4d90fe;
+  }
+`;
